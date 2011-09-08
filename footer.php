7b68ec6e5a2947654d1d2bbff157473bacf6d397
Ajay Sharma <dudleydragon@hotmail.com>
2011-09-07 23:43:28 -0700

    Adding Stack Overflow link to footer Resorted links to put professional linksfirst

--- footer.php
+++ footer.php
@@ -9,19 +9,20 @@
 
 	<ul class="linkslist">
 		<li><a id="twitter-contact" href="http://twitter.com/ajsharma" rel="me" title="Find me on Twitter" target="_blank">Twitter</a></li>
-		<li><a id="facebook-contact" href="http://www.facebook.com/ajsharma" rel="me" title="Find me on Facebook" target="_blank">Facebook</a></li>
+		<li><a id="stack-overflow-contact" href="http://stackoverflow.com/users/924061/ajsharma" rel="me" title="Find me on Stack Overflow" target="_blank">Stack Overflow</a></li>
 		<li><a id="linkedin-contact" href="http://www.linkedin.com/pub/ajay-sharma/3/525/122" rel="me" title="Find me on LinkedIn" target="_blank">LinkedIn</a></li>
 		<li><a id="git-contact" href="https://github.com/ajsharma" rel="me" title="See my work on GitHub" target="_blank">GitHub</a></li>
+		<li><a id="dribbble-contact" href="http://dribbble.com/ajsharma" rel="me" title="See my designs on Dribbble" target="_blank">Dribbble</a></li>
 		<li><a id="email-contact" href="mailto:aj@ajsharma.com" rel="me" title="Email me" target="_blank">Email</a></li>
 		<li><a id="aim-contact" href="aim:ajsharma01" rel="me" title="IM me" target="_blank">AIM</a></li>
+		<li><a id="ted-contact" href="http://www.ted.com/profiles/favorites/id/300643" rel="me" title="Find me on TED" target="_blank">TED</a></li>
 		<li><a id="tumblr-contact" href="http://ajsharma.tumblr.com/" rel="me" title="Find me on Tumblr" target="_blank">Tumblr</a></li>
 		<li><a id="yelp-contact" href="http://ajsharma.yelp.com/" rel="me" title="Find me on Yelp" target="_blank">Yelp</a></li>
 		<li><a id="pandora-contact" href="http://www.pandora.com/people/ajsharma" rel="me" title="Find me on Pandora" target="_blank">Pandora</a></li>
 		<li><a id="quora-contact" href="http://www.quora.com/Ajay-Sharma-2" rel="me" title="See my answers on Quora" target="_blank">Quora</a></li>
-		<li><a id="dribbble-contact" href="http://dribbble.com/ajsharma" rel="me" title="See my designs on Dribbble" target="_blank">Dribbble</a></li>
 		<li><a id="lastfm-contact" href="http://www.last.fm/user/ajsharma01" rel="me" title="Find me on Last.fm" target="_blank">Last.fm</a></li>
 		<li><a id="digg-contact" href="http://digg.com/users/ajsharma01" rel="me" title="Find me on Digg" target="_blank">Digg</a></li>
-		<li><a id="ted-contact" href="http://www.ted.com/profiles/favorites/id/300643" rel="me" title="Find me on TED" target="_blank">TED</a></li>
+		<li><a id="facebook-contact" href="http://www.facebook.com/ajsharma" rel="me" title="Find me on Facebook" target="_blank">Facebook</a></li>
 	</ul>
 </footer>
 
