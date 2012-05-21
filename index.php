e8c142b5f89681cc10898824c84e7c0131246363
Ajay Sharma <aj@ajsharma.com>
2012-05-21 15:38:34 -0700

    Adding wrapper dive to article content

--- index.php
+++ index.php
@@ -26,14 +26,16 @@ while (have_posts()) : the_post(); ?>
 <article <?php post_class() ?> id="post<?php the_ID(); ?>" >
 	<header>
 		<time pubdate datetime="<?php the_time('c') ?>">
-			<span class="month"><?php the_time('F') ?></span>,
-			<span class="day"><?php the_time('d') ?></span>
+			<span class="month"><?php the_time('F') ?></span>
+			<span class="day"><?php the_time('d') ?></span>,
 			<span class="year"><?php the_time('Y') ?></span>
 		</time>
 		<h1 class="posttitle"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
 	</header>
-
-	<?php the_content('continue reading...'); ?>
+	
+	<div class="article-content">
+		<?php the_content('continue reading...'); ?>
+	</div>
 	
 	<footer>
 	</footer>
