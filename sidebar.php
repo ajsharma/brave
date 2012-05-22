d67e92504bd53958232be9731ec00ded96ab68a8
Ajay Sharma <dudleydragon@hotmail.com>
2012-05-22 15:13:36 -0700

    Adding sidebar link to posts authored by me specifically.

--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,6 @@
 <nav class="global-navigation">  
 	<ul>
 		<?php wp_list_pages('title_li='); ?>
+		<li class="page_item"><a href="http://ajsharma.com/author/ajsharma/">Articles</a></li>
 	</ul>
 </nav>
